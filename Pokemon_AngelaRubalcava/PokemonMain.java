daa4d086bbe7726ff1ee96f6510aa6cc57679003
angelarubalcava <amrubalcava2@dons.usfca.edu>
2021-03-09 19:06:52 -0800

    Added PokemonMain.java

---
+++ Pokemon_AngelaRubalcava/PokemonMain.java
@@ -0,0 +1,45 @@
+package class0223;
+
+public abstract class PokemonMain {
+	
+	private String name;
+	private String color;
+	
+	public PokemonMain(String name, String color) {
+		this.name = name;
+		this.color = color;
+		
+	}
+	
+	public PokemonMain() {
+		 this.name = "";
+		 this.color = "";
+		 
+	  }
+	
+	 public String getName() {
+		 return this.name;
+	}
+		  
+	 public String getColor() {
+		return this.color;
+	}
+	 
+	 public void setName(String n) {
+		 this.name = n;
+	}
+		  
+	public void setColor(String c) {
+		this.color = c;
+	} 
+	
+	
+	public abstract void speed();
+	  
+	public abstract void specialAttack();
+	
+	public String toString() {
+	    return "I am a Pokemon: " + this.name + " : " + this.color + " : ";
+	  }
+
+}
